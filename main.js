0de97035003527fa85dfbe084fe38448c936ffb4
ZAMZA <salimzamza@gmail.com>
2024-10-23 01:50:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,85 @@
 import { privateKey } from './wallet.js';
+import { Config } from './config.js';
 import { proxyList } from './proxy.js';
-import a0_0x31881c from './core.js';
+import a0_0x3c7582 from './core.js';
 import { Helper } from './helper.js';
-import a0_0x47ed64 from './logger.js';
-async function operation(_0x5310a6, _0x14a08b, _0x3bc245) {
-  const _0x4a209a = new a0_0x31881c(_0x5310a6, _0x14a08b, _0x3bc245);
+import a0_0x23b1d9 from './logger.js';
+
+async function operation(_0x3fd3ea, _0x558e18, _0x2b69df) {
+  const _0x5deb10 = new a0_0x3c7582(_0x3fd3ea, _0x558e18, _0x2b69df);
   try {
-    await _0x4a209a.connectWallet();
-    await _0x4a209a.getBalance();
-    await _0x4a209a.getPoint(true);
-    await _0x4a209a.connectSonicDapps();
-    await _0x4a209a.getUserInfo();
-    await _0x4a209a.getUserInvite();
-    await _0x4a209a.createSession();
-    await _0x4a209a.permitContract();
-    while (_0x4a209a.limit == false) {
-      await _0x4a209a.playPlinko();
-      await _0x4a209a.getPoint();
+    await _0x5deb10.connectWallet();
+    await _0x5deb10.getBalance();
+    if (Config.DISPLAYPOINT) {
+      await Helper.retryOperation(_0x5deb10.getPoint.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
     }
-    while (_0x4a209a.wheelLimit == false) {
-      await _0x4a209a.playWheel();
-      await _0x4a209a.getPoint();
+    await Helper.retryOperation(_0x5deb10.connectSonicDapps.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+    await Helper.retryOperation(_0x5deb10.getUserInfo.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+    await Helper.retryOperation(_0x5deb10.getUserInvite.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+    await Helper.retryOperation(_0x5deb10.createSession.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+    await Helper.retryOperation(_0x5deb10.permitContract.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+    while (_0x5deb10.limit == false) {
+      await Helper.retryOperation(_0x5deb10.playPlinko.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      if (Config.DISPLAYPOINT) {
+        await Helper.retryOperation(_0x5deb10.getPoint.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      }
     }
-    while (_0x4a209a.mineLimit == false) {
-      await _0x4a209a.playMine();
-      await _0x4a209a.getPoint();
+    while (_0x5deb10.wheelLimit == false) {
+      await Helper.retryOperation(_0x5deb10.playWheel.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      if (Config.DISPLAYPOINT) {
+        await Helper.retryOperation(_0x5deb10.getPoint.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      }
     }
-    const _0x4963d1 = privateKey.find(_0x1e6bb8 => _0x1e6bb8.pk == _0x5310a6);
-    const _0x5bf050 = privateKey.indexOf(_0x4963d1);
-    await Helper.delay(43200000, _0x5310a6, "Account " + (_0x5bf050 + 0x1) + " Processing Done, Delaying for " + Helper.msToTime(43200000), _0x4a209a);
-    await operation(_0x5310a6, _0x14a08b, _0x3bc245);
-  } catch (_0x54ffd5) {
-    if (_0x54ffd5.message) {
-      await Helper.delay(0x2710, _0x5310a6, "Error : " + _0x54ffd5.message + ", Retry again after 10 Second", _0x4a209a);
+    while (_0x5deb10.mineLimit == false) {
+      await Helper.retryOperation(_0x5deb10.playMine.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      if (Config.DISPLAYPOINT) {
+        await Helper.retryOperation(_0x5deb10.getPoint.bind(_0x5deb10, false), _0x3fd3ea, _0x5deb10);
+      }
+    }
+    const _0x1c23c3 = privateKey.find(_0x56e610 => _0x56e610.pk == _0x3fd3ea);
+    const _0x5428f4 = privateKey.indexOf(_0x1c23c3);
+    await Helper.delay(43200000, _0x3fd3ea, "Account " + (_0x5428f4 + 0x1) + " Done ", _0x5deb10);
+    await operation(_0x3fd3ea, _0x558e18, _0x2b69df);
+  } catch (_0x2ba9a3) {
+    if (_0x2ba9a3.message) {
+      await Helper.delay(0x2710, _0x3fd3ea, "Error : " + _0x2ba9a3.message + ", Retry in 10 Second", _0x5deb10);
     } else {
-      await Helper.delay(0x2710, _0x5310a6, "Error :" + JSON.stringify(_0x54ffd5) + ", Retry again after 10 Second", _0x4a209a);
+      await Helper.delay(0x2710, _0x3fd3ea, "Error :" + JSON.stringify(_0x2ba9a3) + ", Retry in 10 Second", _0x5deb10);
     }
-    await operation(_0x5310a6, _0x14a08b, _0x3bc245);
+    await operation(_0x3fd3ea, _0x558e18, _0x2b69df);
   }
 }
+
 async function startBot() {
-  return new Promise(async (_0x78005b, _0x42b8bc) => {
+  return new Promise(async (_0xf5abe0, _0xca11db) => {
     try {
-      a0_0x47ed64.info("BOT STARTED");
-      const _0x5b2fa1 = [];
+      a0_0x23b1d9.info("BOT STARTED");
+      const _0x19cd86 = [];
       if (proxyList.length != privateKey.length && proxyList.length != 0x0) {
         throw Error("You Have " + privateKey.length + " Accounts But Provide " + proxyList.length);
       }
-      for (const _0x177973 of privateKey) {
-        if (!_0x177973.pk) {
-          throw Error("Your accounts.js is malformed, please fix it first, see accounts_tmp.js for the format");
+      for (const _0x391d30 of privateKey) {
+        if (!_0x391d30.pk) {
+          throw Error("please check your account on wallet.js");
         }
-        const _0x154402 = privateKey.indexOf(_0x177973);
-        const _0x2d36a0 = proxyList[_0x154402];
-        _0x5b2fa1.push(operation(_0x177973.pk, _0x177973.smartWalletAddress, _0x2d36a0));
+        const _0x4c9576 = privateKey.indexOf(_0x391d30);
+        const _0x5af089 = proxyList[_0x4c9576];
+        _0x19cd86.push(operation(_0x391d30.pk, _0x391d30.smartWalletAddress, _0x5af089));
       }
-      await Promise.all(_0x5b2fa1);
-      _0x78005b();
-    } catch (_0x4b4164) {
-      a0_0x47ed64.info("BOT STOPPED");
-      a0_0x47ed64.error(JSON.stringify(_0x4b4164));
-      _0x42b8bc(_0x4b4164);
+      await Promise.all(_0x19cd86);
+      _0xf5abe0();
+    } catch (_0x492d39) {
+      a0_0x23b1d9.info("BOT STOPPED");
+      a0_0x23b1d9.error(JSON.stringify(_0x492d39));
+      _0xca11db(_0x492d39);
     }
   });
 }
+
 (async () => {
   try {
-    a0_0x47ed64.clear();
-    a0_0x47ed64.info('');
+    a0_0x23b1d9.clear();
+    a0_0x23b1d9.info('');
     Helper.ASC();
     await startBot();
   } catch (_0x11fe99) {
